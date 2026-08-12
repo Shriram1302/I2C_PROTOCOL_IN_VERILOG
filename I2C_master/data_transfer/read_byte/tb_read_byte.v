7a6929ceefc23d7dab053c91148943073d84ba51
Shriramachandran R S <shriramachandranrs.ec24@bitsathy.ac.in>
2026-08-12 15:51:15 +0530

    Update tb_read_byte.v

--- I2C_master/data_transfer/read_byte/tb_read_byte.v
+++ I2C_master/data_transfer/read_byte/tb_read_byte.v
@@ -11,18 +11,12 @@ wire sck;
 wire done;
 wire [7:0] rx_byte;
 
-//------------------------------------
-// Slave Drive
-//------------------------------------
+
 reg slave_drive;
 
-// Byte to transmit from slave
 reg [7:0] tx_byte;
 integer i;
-
-//------------------------------------
-// DUT
-//------------------------------------
+-
 read_byte dut(
     .stat_en(stat_en),
     .clk(clk),
@@ -33,45 +27,37 @@ read_byte dut(
     .done(done)
 );
 
-//------------------------------------
-// Open Drain Bus
-//------------------------------------
+
 assign sda = slave_drive ? 1'b0 : 1'bz;
 
 pullup(sda);
 pullup(sck);
 
-//------------------------------------
-// 400 kHz Clock
-//------------------------------------
+
 initial
     clk = 0;
 
 always
     #1250 clk = ~clk;
 
-//------------------------------------
-// Slave Data Generator
-//------------------------------------
+
 initial begin
 
     slave_drive = 0;
-    tx_byte = 8'hA5;      //10100101
+    tx_byte = 8'hA5;      
 
     wait(stat_en);
 
-    // Send MSB first
     for(i=7;i>=0;i=i-1) begin
 
-        // Wait until SCL goes LOW
+
         @(negedge sck);
 
         if(tx_byte[i] == 1'b0)
-            slave_drive = 1;      //Drive LOW
+            slave_drive = 1;      
         else
-            slave_drive = 0;      //Release HIGH
+            slave_drive = 0;     
 
-        // Wait until SCL HIGH
         @(posedge sck);
 
     end
@@ -80,9 +66,7 @@ initial begin
 
 end
 
-//------------------------------------
-// Test Sequence
-//------------------------------------
+
 initial begin
 
     rst = 0;
